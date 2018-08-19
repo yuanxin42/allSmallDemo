0f05662cfbbbbc9c66284cd00fa2ed62b5aa9af6
unknown <l>
2018-08-19 17:42:17 +0800

    init pro

---
+++ 1.js
@@ -0,0 +1,16 @@
+{
+    tag: 'div',
+    children: [
+        {
+            isStatic: true,
+            isComment: true,
+            tag: 'a',
+            text: 'share'
+        }
+    ]
+}
+
+
+<div>
+    {/* <a>share</a> */}
+</div>
